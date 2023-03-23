63488aa394d36eda56810b5b641ec97a72acd612
Edward Zota <zotaedward@gmail.com>
2023-03-23 18:03:24 +0100

    esercizio concluso + aggiunto bottone e anche la possibilita di scegliere quante mail generare + un po di css

--- js/myscript.js
+++ js/myscript.js
@@ -3,15 +3,18 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
+        numeroMail:'',
         lista:[]
       }
     },
-    mounted(){
-      for(let i=0;i<10;i++){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-          this.lista.push({email:response.data.response});
-        });
+    methods:{
+      generatorMail(){
+        for(let i=0;i<this.numeroMail;i++){
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(response => {
+            this.lista.push({email:response.data.response});
+          });
+        }
       }
     }
   }).mount('#app')
